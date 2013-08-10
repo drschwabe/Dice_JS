2aaeb6e7dcff8a6d38223bdb4d9659af520d2140
Derrick Schwabe <admin@mrschwabe.com>
2013-08-10 10:34:53 -0600

    Timed toggling/CSS animations of each side of the die.

--- js/Dice.js
+++ js/Dice.js
@@ -4,7 +4,34 @@ $(document).ready(function() {
 
 	var die = $('#die'); 
 
-	die.addClass('show-bottom'); 
+	//Animate the die after 1 second: 
+	setTimeout(function () {
+		die.addClass('show-bottom'); 
+	}, 1000); 
 
+	setTimeout(function () {
+		die.removeClass(); 
+		die.addClass('show-top'); 
+	}, 2000); 	
+
+	setTimeout(function () {
+		die.removeClass(); 		
+		die.addClass('show-left'); 
+	}, 3000); 	
+
+	setTimeout(function () {
+		die.removeClass(); 		
+		die.addClass('show-right'); 
+	}, 4000); 	
+
+	setTimeout(function () {
+		die.removeClass(); 		
+		die.addClass('show-front'); 
+	}, 5000); 
+
+	setTimeout(function () {
+		die.removeClass(); 		
+		die.addClass('show-back'); 
+	}, 6000); 			
 
 }); 
