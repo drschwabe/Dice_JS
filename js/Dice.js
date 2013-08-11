d0227226bea03f0a2064e04b1eadb552006f3480
Derrick Schwabe <admin@mrschwabe.com>
2013-08-11 13:35:12 -0600

    Added Gameplay UI and button logic.

--- js/Dice.js
+++ js/Dice.js
@@ -8,21 +8,30 @@ $(document).ready(function() {
 	//Define UI elements: 
 	var die = $('#die'); 
 
+	$('.screen').hide(); 
 	$('#startScreen').show(); 
-	$('#dieScreen').hide(); 
 
 
 	//############# AI ################
 
 	function Ai(command) {
+
+		if (command == 'Start') {
+			$('.screen').hide(); 			
+			$('#startScreen').show(); 
+		}
+
 		if (command == 'Roll') {
 			$('#startScreen').hide(); 
 			$('#dieScreen').show();
+			$('#gameplayUI').show();	
+
+			die.removeClass(); 
+			die.addClass('default'); 			
 
 			setTimeout(function () {
 				die.addClass('spin'); 
 				}, 1000); 
-
 
 			setTimeout(function () {
 				die.addClass('show-bottom'); 
@@ -35,8 +44,18 @@ $(document).ready(function() {
 
 	//############# INPUT ################
 
+	/* Main menu: */
 	$('#rollBtn').click(function() {
 		Ai('Roll')
 	}); 
+
+	/* Gameplay: */
+	$('#homeBtn').click(function() {
+		Ai('Start')
+	}); 	
+
+	$('#rollAgainBtn').click(function() {
+		Ai('Roll')
+	}); 	
 
 });
