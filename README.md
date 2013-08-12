b3456064ebbf00f44e130c5b04052df7cc3f6d78
Derrick Schwabe <admin@mrschwabe.com>
2013-08-12 17:50:39 -0600

    Improved code comments and updated readme.

--- README.md
+++ README.md
@@ -1,38 +1,8 @@
-**[Demo](http://htmlpreview.github.io/?https://github.com/mrschwabe/Dice_Haxe_OpenFL-HTML5/blob/master/targets/html5/bin/dice.html)**
-
-### Compile info ###
-
-Project is coded in Haxe (Haxe/AS3/JS/jQuery) and targets HTML5 via OpenFL. I also made some tweaks to the standard OpenFL workflow in order to do UI and graphics in HTML/CSS.  
-
-Install **[Haxe](http://haxe.org/download)**<br />
-Instal **[OpenFL](https://github.com/openfl/openfl/wiki/Get-Started)**
-
-
-Clone repo and run: 
-
-    openfl test html5
-    
-
-Run **dice.html**
-
-OpenFL will generate an index.html file each build.  You can ignore that - instead use the dice.html template which is already setup (without an existing template, you would have to write/generate the UI in Haxe). 
-
-(tested on Windows)
-<br /><br />
-
-**Helpful links**: <br />
-http://haxe.org<br />
-http://api.haxe.org<br />
-http://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/<br />
-https://github.com/openfl/openfl/wiki/Get-Started
-
-
-<br />
 
 
 
 ### Premise ###
-This is a practice <del>Ruby Haxe</del> Haxe + OpenFL project.  To simulate... DICE. 
+This is a practice <del>Ruby Haxe Haxe + OpenFL </del>JavaScript / CSS3 project.  To simulate... DICE. 
 What can a die do?  Well. It can roll. 
 What properties are of it?  It has 6 sides (typically, though some DnD ones have more). What happens when a die is rolled?  It lands!  One one of the six sides. The die then shows the result.  And the roll is complete.  So we have a begin state. A rolling state.  Rolls typically take a few seconds.  And the final result state.  In which the roll is complete, and the result is displayed for us all to see. 
 
@@ -51,3 +21,13 @@ Application's execution goes something like this:
 **Input**() accepts user input, translates it for Ai. 
 
 Organizing & separating code into these three paradigms, then enforcing consistent flow between them - seems to help one keep a good mental model of the app's execution flow. Or maybe it's just CRA-ZAY.
+
+<br />
+
+### Links & reference ###
+
+For the homescreen UI animations:<br />
+[http://daneden.me/animate/ ](http://daneden.me/animate/)
+
+And for an excellent primer on 3D CSS: <br />
+[http://desandro.github.io/3dtransforms/](http://desandro.github.io/3dtransforms/)
--- js/Dice.js
+++ js/Dice.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 	var die = $('#die'); 
 
 
+	//A function to update the content/style of the Roll button; changes depending on if the die is rolling or not: 
 	function updateRollButton() {
 		var rollAgainBtn = $('#rollAgainBtn'); 
 		if(rolling == true ) { 
@@ -17,7 +18,7 @@ $(document).ready(function() {
 		}
 	}
 
-	//main function:
+	//Main function to render the screen: 
 	function Display(screen) {
 		
 		//Log the current screen Ai can keep track of where we are: 		
@@ -32,18 +33,17 @@ $(document).ready(function() {
 			console.log("Welcome to DICE.");			
 			$('#startScreen').show(); 
 
-
 			//Animate UI elements: 
 			$("#rollBtn ").addClass("animated bounceInUp"); 
 			$("#logo").addClass('animated bounceIn'); 
-
-
 		}
 
 		if (screen == 'Rolling') {
+			//Hide start screen and show the gameplay screens: 
 			$('#startScreen').hide(); 
 			$('#dieScreen').show();
 			$('#gameplayUI').show();
+			//Make the roll button say 'ROLLING': 
 			updateRollButton(); 			
 		}
 	}
@@ -57,6 +57,7 @@ $(document).ready(function() {
 	var spinToClass = 'spintToFront'; 
 	var rolling = false; 
 
+	//A function to determine the appropriate class to apply to the 3D die depending on the result of the user's roll: 
 	function determineDieClass(result, status) {
 
 		if(status == 'spinTo') { 
@@ -109,56 +110,60 @@ $(document).ready(function() {
 	}	
 
 
-	//Primary function: 
+	//Primary logic function: 
 	function Ai(command) {
 
 		if (command == 'Start') {
+			//Set a brief lag after the user taps the roll button for the first time: 
 			delayTime = 600;
+
+			//Call the main Display function to render the needed screen: 
 			Display('Starting');
 		}
 
 		if (command == 'Roll' && rolling == false) {
 
+			//Now we rolling! 
 			rolling = true; 
 
 			//Perform the dice roll computation: 
 			rollResult = Math.ceil(Math.random() * 6 - 1) + 1;
 			console.log(rollResult); 
 
+			//Display rolling screen...
+			Display('Rolling');
 
-			//Display rolling screen: 
-			Display('Rolling'); 			
-
-			
-			//In one second, initiate the spin animation...
+			//and after a brief moment, initiate the spin animation...
 			setTimeout(function () {
 				die.addClass(spinFromClass); 
 				}, delayTime); 
 
 	
-			//And add the final class: 
+			//then after some spinning, 'spin to' the class for the die's landing state...
 			setTimeout(function() {
 
-				//Determine the final class based on the rollResult..
+				//Determine this final class based on the rollResult..
 				spinToClass = determineDieClass(rollResult, 'spinTo'); 
 
-				//Replace the previously set class with the new one:  				
+				//and replace the previously set class with this new one:  				
 				die.attr('class', spinToClass); 
 
 			}, delayTime + 1500);  
 
 
+			//and after the final spin completes, do some housekeeping: 
 			setTimeout(function() {
 				die.removeClass('spin'); 
-				//Based on the result, determine a class we start from for the next roll (this ensures animation starts from the last completed roll): 
+
+				//Based on the result, determine a class we start from for the next roll (this ensures the next animation starts from the last completed roll): 
 				spinFromClass = determineDieClass(rollResult, 'spinFrom'); 	
 
 				//Update the delay time to reduce it for subsequent rolls: 
 				delayTime = 0; 
 
-	
-				//Conclude by updating rolling status.., 
+				//Conclude by updating rolling status.. 
 				rolling = false; 
+
 				//and have display update the Roll Again button content: 
 				updateRollButton(); 
 
@@ -173,7 +178,6 @@ $(document).ready(function() {
 
 	/* Main menu: */
 	$('#rollBtn, #rollAgainBtn').click(function() {
-
 		Ai('Roll'); 
 	}); 
 
@@ -182,7 +186,6 @@ $(document).ready(function() {
 		Ai('Start')
 	}); 	
 	
-
 	Ai('Start'); 
 
 });
